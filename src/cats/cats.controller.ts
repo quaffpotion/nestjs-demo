6ff6fd01a6f1ecbcd50aac25e486c4e1aa05c2c9
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-09-17 19:40:04 -0500

    can grab request object, note too big to serialize so we log it

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   foo(@Req() request: Request): string {
-    return 'This actiasdfasdfon returns all cats';
+    console.log(request);
+    return 'asdfasdfa';
   }
 }
